e7f7b2195d8270b47146bac36068bddef453f192
matma6 <matma6@matma6.net>
2016-12-02 17:15:36 +0100

    Działa rysowanie i wychodzenie

--- gra.rb
+++ gra.rb
@@ -3,25 +3,31 @@ require 'gosu'
 class GameWindow < Gosu::Window
 	def initialize
 		super 640, 480
-		self.caption = "Gosu Tutorial Game"
+		self.caption = "Kółko i krzyżyk"
 
 		@x = Gosu::Image.new "grafiki/x.png"
 		@o = Gosu::Image.new "grafiki/o.png"
 
 		@plansza = [
-			[:puste, :puste, :puste],
-			[:puste, :puste, :puste],
-			[:puste, :puste, :puste]
+			[:puste, :x, :puste],
+			[:puste, :x, :puste],
+			[:puste, :o, :x]
 		]
 	end
 
 	def update
+		close if Gosu::button_down? Gosu::KbEscape
 	end
 
 	def draw
 		@plansza.each_with_index do |wiersz, y|
 			wiersz.each_with_index do |komorka, x|
-				@x.draw 64*x, 64*y, 0
+				if komorka == :puste
+				elsif komorka == :x
+					@x.draw 64*x, 64*y, 0
+				else
+					@o.draw 64*x, 64*y, 0
+				end
 			end
 		end
 	end
